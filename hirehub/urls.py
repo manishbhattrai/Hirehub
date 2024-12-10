4f5f108fd927bf4de5b32022e23f5f168d036d5f
LazyManish <mbhattarai900@gmail.com>
2024-12-10 07:39:04 +0545

    modified

--- hirehub/urls.py
+++ hirehub/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.user_create, name='signup'),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('logout/',views.users_logout, name='logout'),
     path('profile/setup/', views.profile_setup, name="profile_setup"),
     path('profile/',views.user_profile, name='profile'),
+    path('update/<int:id>',views.profile_update, name='update'),
+    path('delete/<int:id>',views.user_delete, name='delete'),
 ]
--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hirehub.urls')),
-    path('',include('userprofile.urls')),
 ] 
 
 if settings.DEBUG:
