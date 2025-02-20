df6d5bc649d0681ca09f6f7c68d9834c54148ba7
LazyManish <mbhattarai900@gmail.com>
2025-02-20 12:40:00 +0545

    addded admin view

--- hirehub/admin.py
+++ hirehub/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Skill)
+admin.site.register(UserProfile)
+
+
+
+
